1c81290d3b281acdd9cede4f1b7aa45a6a01ac99
Rakesh Shrestha <rakeshshrestha3222@gmail.com>
2021-04-16 22:47:05 +0545

    With error message also

--- js/script.js
+++ js/script.js
@@ -1,28 +1,41 @@
 function getvalue(val) {
     document.getElementById("text").value += val;// to display in calculator
 }
+
 function clears() {
     document.getElementById("text").value = " ";//to clear data when C button is pressed
     document.getElementById("ans").value = " ";
 }
 
 function solve() {
-    let x = document.getElementById("text").value;//we get the input from user and is assigned in x
-    let y = eval(x);//the input is evaluated
-
-        if (typeof y === "undefined") {
-            alert("Please insert value !");
-          } 
-        // else if (isNaN(y)){
-        //    alert("Please select number !");
-        //  }
-
-
-
-        else {
-            document.getElementById("ans").value = "=" + y;//the soln is printed in screen
-        }
-
     
-   
+    try{
+        let x = document.getElementById("text").value;//we get the input from user and is assigned in x
+        let y = eval(x);//the input is evaluated
+        //document.getElementById("ans").style.color = "#ff0000";
+        
+        if (typeof y === "undefined") {
+            //alert("Please insert value !");
+            document.getElementById("ans").style.color = "#ff0000";
+            document.getElementById("ans").value = " Please insert value !";   
+          } 
+        else if (isNaN(y)){
+           //alert("Mathematical Error");
+           document.getElementById("ans").style.color = "#ff0000";
+           document.getElementById("ans").value = " Mathematical Error !"
+         }
+        else {
+            document.getElementById("ans").style.color = "green";
+           document.getElementById("ans").value = "=" + y;//the soln is printed in screen
+        }
+    }
+    catch (err){
+        //alert('Invalid !');
+        document.getElementById("ans").style.color = "#ff0000";
+        document.getElementById("ans").value = " Invaild ! ";
+    }
+    
+
 }
+
+
